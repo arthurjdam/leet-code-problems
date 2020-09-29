dc468fdd9b207eb0263056fbf73cf46141b4d17a
arthurjdam <arthurjdam@gmail.com>
2020-09-29 15:44:02 -0700

    Add letter-combinations-of-a-phone-number

---
+++ src/letter-combinations-of-a-phone-number.ts
@@ -0,0 +1,13 @@
+const keys: Record<string, string> = {
+  '1': '',
+  '2': 'abc',
+  '3': 'def',
+  '4': 'ghi',
+  '5': 'jkl',
+  '6': 'mno',
+  '7': 'pqrs',
+  '8': 'tuv',
+  '9': 'wxyz',
+};
+
+export default function letterCombinations(digits: string): string[] {}
