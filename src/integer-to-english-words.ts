a93946801c6a2eb0216219b094ab7cdf9e4a3b4f
arthurjdam <arthurjdam@gmail.com>
2020-09-30 13:52:36 -0700

    better solution

--- src/integer-to-english-words.ts
+++ src/integer-to-english-words.ts
@@ -1,61 +1,81 @@
 const singles = [
-    '',
-    'One',
-    'Two',
-    'Three',
-    'Four',
-    'Five',
-    'Six',
-    'Seven',
-    'Eight',
-    'Nine'
+  '',
+  'One',
+  'Two',
+  'Three',
+  'Four',
+  'Five',
+  'Six',
+  'Seven',
+  'Eight',
+  'Nine',
 ];
 const exceptions: Record<number, string> = {
-    11: 'Eleven',
-    12: 'Twelve',
-    13: 'Thirteen',
-    14: 'Fourteen',
-    15: 'Fifteen',
-    16: 'Sixteen',
-    17: 'Seventeen',
-    18: 'Eighteen',
-    19: 'Nineteen',
+  11: 'Eleven',
+  12: 'Twelve',
+  13: 'Thirteen',
+  14: 'Fourteen',
+  15: 'Fifteen',
+  16: 'Sixteen',
+  17: 'Seventeen',
+  18: 'Eighteen',
+  19: 'Nineteen',
 };
 
 const minors = [
-    '',
-    '',
-    'Twenty',
-    'Thirty',
-    'Forty',
-    'Fifty',
-    'Sixty',
-    'Seventy',
-    'Eighty',
-    'Ninety',
+  '',
+  '',
+  'Twenty',
+  'Thirty',
+  'Forty',
+  'Fifty',
+  'Sixty',
+  'Seventy',
+  'Eighty',
+  'Ninety',
 ];
 
 function hundred(num: number): string {
-    num = num % 10 ** 3;
+  num = num % 10 ** 3;
+  const out = [];
 
-    if (num < 10) return singles[num];
-    if (num < 10 ** 2) {
-        if (exceptions[num]) {
-            return exceptions[num];
-        }
-        return `${minors[Math.floor(num / 10)]} ${singles[num % 10]}`;
+  if (num > 10 ** 2) {
+    const hundreds = Math.floor(num / 100);
+    if (exceptions[hundreds]) {
+      out.push(exceptions[hundreds]);
+    } else {
+      out.push(singles[hundreds]);
     }
-
-    return `${singles[Math.floor(num / 100)]} Hundred ${minors[Math.floor(num / 10) % 10]} ${singles[num % 10]}`;
+    out.push('Hundred');
+  }
+  if (num > 10) {
+    if (exceptions[num % 100]) {
+      out.push(exceptions[num % 100]);
+    } else if (num % 100 > 0) {
+      out.push(minors[Math.floor((num / 10) % 10)]);
+      if (num % 10 > 0) {
+        out.push(singles[num % 10]);
+      }
+    }
+  } else if (num % 10 > 0) {
+    out.push(singles[num % 10]);
+  }
+  return out.join(' ');
 }
 
-export default function numberToWords(num: number): string {    
-    if (num < 10 ** 3) {
-        return hundred(num);
-    } else if (num < 10 ** 6) {
-        return `${hundred(Math.floor(num / 10 ** 3))} Thousand ${hundred(num)}`; 
-    } else if (num < 10 ** 9) {
-        return `${hundred(Math.floor(num / 10 ** 6))} Million ${hundred(Math.floor(num / 10 ** 3))} Thousand ${hundred(num)}`; 
-    }
-    return `${hundred(Math.floor(num / 10 ** 9))} Billion ${hundred(Math.floor(num / 10 ** 6))} Million ${hundred(Math.floor(num / 10 ** 3))} Thousand ${hundred(num)}`;
-};
+export default function numberToWords(num: number): string {
+  const out = [];
+  if (hundred(Math.floor(num / 10 ** 9)).length > 0) {
+    out.push(hundred(Math.floor(num / 10 ** 9)), 'Billion');
+  }
+  if (hundred(Math.floor(num / 10 ** 6)).length > 0) {
+    out.push(hundred(Math.floor(num / 10 ** 6)), 'Million');
+  }
+  if (hundred(Math.floor(num / 10 ** 3)).length > 0) {
+    out.push(hundred(Math.floor(num / 10 ** 3)), 'Thousand');
+  }
+  if (hundred(Math.floor(num)).length > 0) {
+    out.push(hundred(Math.floor(num)));
+  }
+  return out.join(' ');
+}
