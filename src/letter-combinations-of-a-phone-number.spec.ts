8d70503391dcf2fe77cfaf1947f2d33de5c69eb2
arthurjdam <arthurjdam@gmail.com>
2020-09-29 17:04:39 -0700

    Add answer for letter-combinations-of-a-phone-number

--- src/letter-combinations-of-a-phone-number.spec.ts
+++ src/letter-combinations-of-a-phone-number.spec.ts
@@ -2,5 +2,6 @@ import { expect, test } from '@jest/globals';
 import letterCombinations from './letter-combinations-of-a-phone-number';
 
 test('Basic examples', () => {
-  expect(letterCombinations('23')).toBe(['ad', 'ae', 'af', 'bd', 'be', 'bf', 'cd', 'ce', 'cf']);
+  expect(letterCombinations('23').sort()).toStrictEqual(['ad', 'ae', 'af', 'bd', 'be', 'bf', 'cd', 'ce', 'cf']);
+  expect(letterCombinations('').sort()).toStrictEqual([]);
 });
--- src/letter-combinations-of-a-phone-number.ts
+++ src/letter-combinations-of-a-phone-number.ts
@@ -1,13 +1,34 @@
-const keys: Record<string, string> = {
-  '1': '',
-  '2': 'abc',
-  '3': 'def',
-  '4': 'ghi',
-  '5': 'jkl',
-  '6': 'mno',
-  '7': 'pqrs',
-  '8': 'tuv',
-  '9': 'wxyz',
+const keys: Record<string, string[]> = {
+  '2': ['a', 'b', 'c'],
+  '3': ['d', 'e', 'f'],
+  '4': ['g', 'h', 'i'],
+  '5': ['j', 'k', 'l'],
+  '6': ['m', 'n', 'o'],
+  '7': ['p', 'q', 'r', 's'],
+  '8': ['t', 'u', 'v'],
+  '9': ['w', 'x', 'y', 'z'],
 };
 
-export default function letterCombinations(digits: string): string[] {}
+function appendSet(digits: string[]): string[] {
+  const digit = digits.pop() as string;
+  const x = keys[digit];
+  if (digits.length > 0) {
+    const y = appendSet(digits);
+    const out = [];
+    for (let x_ = 0; x_ < x.length; ++x_) {
+      for (let y_ = 0; y_ < y.length; ++y_) {
+        out.push(`${y[y_]}${x[x_]}`);
+      }
+    }
+    return out;
+  }
+  return x;
+}
+
+export default function letterCombinations(digits: string): string[] {
+  const splitDigits = digits
+    .split('')
+    .filter((digit) => digit !== '0' && digit !== '1');
+  if (splitDigits.length <= 0) return [];
+  return appendSet(splitDigits);
+}
