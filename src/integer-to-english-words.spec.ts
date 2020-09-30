b154f9fc081125294b1d95c84115085fe3fcb81c
arthurjdam <arthurjdam@gmail.com>
2020-09-30 13:17:50 -0700

    Extra tests

--- src/integer-to-english-words.spec.ts
+++ src/integer-to-english-words.spec.ts
@@ -6,7 +6,12 @@ test('Integer to English Words', () => {
     expect(numberToWords(12)).toBe('Twelve');
     expect(numberToWords(24)).toBe('Twenty Four');
     expect(numberToWords(123)).toBe('One Hundred Twenty Three');
+    expect(numberToWords(411)).toBe('Four Hundred Eleven');
+    expect(numberToWords(20000)).toBe('Twenty Thousand');
     expect(numberToWords(12345)).toBe('Twelve Thousand Three Hundred Forty Five');
+    expect(numberToWords(200000)).toBe('Two Hundred Thousand');
+    expect(numberToWords(200400)).toBe('Two Hundred Thousand Four Hundred');
     expect(numberToWords(1234567)).toBe('One Million Two Hundred Thirty Four Thousand Five Hundred Sixty Seven');
     expect(numberToWords(1234567891)).toBe('One Billion Two Hundred Thirty Four Million Five Hundred Sixty Seven Thousand Eight Hundred Ninety One');
+    expect(numberToWords(1000000000)).toBe('One Billion');
 });
