6d5c30d729e0dbecbe5a5dfd2397354615515667
arthurjdam <arthurjdam@gmail.com>
2020-10-01 16:57:57 -0700

    add-to-array-form answer v1

--- src/add-to-array-form-of-integer.ts
+++ src/add-to-array-form-of-integer.ts
@@ -1,1 +1,3 @@
-export default function addToArrayForm(A: number[], K: number): number[] {}
+export default function addToArrayForm(A: number[], K: number): number[] {
+    return String(parseInt(A.join(''), 10) + K).split('').map(s => parseInt(s, 10));
+}
