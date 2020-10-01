0d4f8d90ba85b33d531a665dab0f4674aff40c4f
arthurjdam <arthurjdam@gmail.com>
2020-10-01 14:27:58 -0700

    shifting-letters take 1

--- src/shifting-letters.spec.ts
+++ src/shifting-letters.spec.ts
@@ -1,6 +1,8 @@
-import {expect, test} from '@jest/globals';
+import { expect, test } from '@jest/globals';
 import shiftingLetters from './shifting-letters';
 
 test('Input examples', () => {
-    expect(shiftingLetters('abc', [3,5,9])).toBe('rpl');
+  expect(shiftingLetters('a', [3])).toBe('d');
+  expect(shiftingLetters('abc', [3, 5, 9])).toBe('rpl');
+  expect(shiftingLetters('bad', [10, 20, 30])).toBe('jyh');
 });
--- src/shifting-letters.ts
+++ src/shifting-letters.ts
@@ -1,3 +1,17 @@
+function offset(char: string, amount: number): string {
+  const charCode = char.charCodeAt(0);
+  if (charCode + amount > 122) {
+    return String.fromCharCode(((charCode + amount) % 122) + 97);
+  }
+  return String.fromCharCode(charCode + amount);
+}
+
 export default function shiftingLetters(S: string, shifts: number[]): string {
-    return '';
+  const str = S.split('');
+  for (let i = 0; i < shifts.length; ++i) {
+    for (let x = 0; x <= i; ++x) {
+      str[x] = offset(str[x], shifts[i]);
+    }
+  }
+  return str.join('');
 }
