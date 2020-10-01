71af55df29f3edb8bce04fb59cf505079d596f5c
arthurjdam <arthurjdam@gmail.com>
2020-10-01 14:48:43 -0700

    Lint

--- src/search-suggestions-system.ts
+++ src/search-suggestions-system.ts
@@ -1,16 +1,19 @@
-export default function suggestedProducts(products: string[], searchWord: string): string[][] {
-    const out: Array<Array<string>> = [];
-    products = products.sort();
-    for(let i = 1; i <= searchWord.length; ++i) {
-        let tmp = [];
-        const currentWord = searchWord.substr(0, i);
-        for(let j = 0; j < products.length; ++j) {
-            if(products[j].substr(0, i) === currentWord) {
-                tmp.push(products[j]);
-            }
-        }
-        if (tmp.length > 3) tmp = tmp.slice(0, 3);
-        out.push(tmp);
+export default function suggestedProducts(
+  products: string[],
+  searchWord: string,
+): string[][] {
+  const out: Array<Array<string>> = [];
+  products = products.sort();
+  for (let i = 1; i <= searchWord.length; ++i) {
+    let tmp = [];
+    const currentWord = searchWord.substr(0, i);
+    for (let j = 0; j < products.length; ++j) {
+      if (products[j].substr(0, i) === currentWord) {
+        tmp.push(products[j]);
+      }
     }
-    return out;
+    if (tmp.length > 3) tmp = tmp.slice(0, 3);
+    out.push(tmp);
+  }
+  return out;
 }
