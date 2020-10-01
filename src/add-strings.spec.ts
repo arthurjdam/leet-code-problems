604c917bd9a8ed513b64180bf84a83f21132252d
arthurjdam <arthurjdam@gmail.com>
2020-10-01 15:02:51 -0700

    Add-strings output should be strings too

--- src/add-strings.spec.ts
+++ src/add-strings.spec.ts
@@ -2,7 +2,7 @@ import { expect, test } from '@jest/globals';
 import addStrings from './add-strings';
 
 test('Examples', () => {
-  expect(addStrings('1', '2')).toBe(3);
-  expect(addStrings('123', '4')).toBe(127);
-  expect(addStrings('10000', '2')).toBe(10002);
+  expect(addStrings('1', '2')).toBe('3');
+  expect(addStrings('123', '4')).toBe('127');
+  expect(addStrings('10000', '2')).toBe('10002');
 });
