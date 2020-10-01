3d98af1992665997fff743852608f83c92ff136d
arthurjdam <arthurjdam@gmail.com>
2020-10-01 14:04:48 -0700

    zigzag-conversion acceptable but slow

--- src/zigzag-conversion.spec.ts
+++ src/zigzag-conversion.spec.ts
@@ -2,6 +2,10 @@ import {expect, test} from '@jest/globals';
 import convert from './zigzag-conversion';
 
 test('Input examples', () => {
+    expect(convert('A', 1)).toBe('A');
+    expect(convert('A', 3)).toBe('A');
+    expect(convert('ABC', 2)).toBe('ACB');
+    expect(convert('ABC', 1)).toBe('ABC');
     expect(convert('PAYPALISHIRING', 3)).toBe('PAHNAPLSIIGYIR');
     expect(convert('PAYPALISHIRING', 4)).toBe('PINALSIGYAHRPI');
 });
--- src/zigzag-conversion.ts
+++ src/zigzag-conversion.ts
@@ -1,3 +1,30 @@
-export default function convert(s: string, numRows: number): string {
+function flat(arr: any[]) {
+  return arr.reduce((acc, val) => acc.concat(val), []);
+}
 
-};
+export default function convert(s: string, numRows: number): string {
+  if (s.length <= 2 || numRows <= 1) return s;
+
+  const arr = new Array(s.length)
+    .fill('')
+    .map(() => new Array(s.length).fill(''));
+
+  const doubleRow = numRows + (numRows - 2);
+
+  for (let i = 0; i < s.length; ++i) {
+    let x = i % doubleRow;
+    let y = Math.floor(i / doubleRow) * 2;
+    if (x >= numRows) {
+      y += 1;
+      x = numRows - (x % numRows) - 2;
+    }
+    arr[x][y] = s[i];
+  }
+
+    // let temp = '';
+    // for (let i = 0; i < arr.length; ++i) {
+    //   temp += arr[i].join(' ') + '\n';
+    // }
+    // console.log(temp);
+  return flat(arr).join('');
+}
