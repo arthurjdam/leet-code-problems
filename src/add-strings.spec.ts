36a01878818ed2edfd752691898f82cd4fb7bbc5
arthurjdam <arthurjdam@gmail.com>
2020-10-01 14:51:22 -0700

    Add add-strings skeleton

---
+++ src/add-strings.spec.ts
@@ -0,0 +1,8 @@
+import { expect, test } from '@jest/globals';
+import addStrings from './add-strings';
+
+test('Examples', () => {
+  expect(addStrings('1', '2')).toBe(3);
+  expect(addStrings('123', '4')).toBe(127);
+  expect(addStrings('10000', '2')).toBe(10002);
+});
---
+++ src/add-strings.ts
@@ -0,0 +1,1 @@
+export default function addStrings(num1: string, num2: string): string {}
