ac20c9f75f9fab36885b99a500fc6eca9c2850f5
arthurjdam <arthurjdam@gmail.com>
2020-10-02 14:19:11 -0700

    answer v1

--- src/longest-palindromic-substring.ts
+++ src/longest-palindromic-substring.ts
@@ -1,1 +1,15 @@
-export default function longestPalindrome(s: string): string {}
+export default function longestPalindrome(s: string): string {
+  let longest = '';
+  for (let i = 0; i < s.length; ++i) {
+    for (let j = i; j < s.length; ++j) {
+      const substr = s.substr(i, s.length - j);
+      if (
+        substr === substr.split('').reverse().join('') &&
+        substr.length > longest.length
+      ) {
+        longest = substr;
+      }
+    }
+  }
+  return longest;
+}
