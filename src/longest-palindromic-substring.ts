685afa6738c04517f1b285734c8908107003f509
arthurjdam <arthurjdam@gmail.com>
2020-10-02 14:19:52 -0700

    Do not test strings shorter than the current answer

--- src/longest-palindromic-substring.ts
+++ src/longest-palindromic-substring.ts
@@ -2,6 +2,7 @@ export default function longestPalindrome(s: string): string {
   let longest = '';
   for (let i = 0; i < s.length; ++i) {
     for (let j = i; j < s.length; ++j) {
+      if (j - i < longest.length) continue;
       const substr = s.substr(i, s.length - j);
       if (
         substr === substr.split('').reverse().join('') &&
