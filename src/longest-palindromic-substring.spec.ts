0fd01966d7c0c69beb6f93fa929539f09ab36685
arthurjdam <arthurjdam@gmail.com>
2020-10-02 14:19:30 -0700

    Additional tests

--- src/longest-palindromic-substring.spec.ts
+++ src/longest-palindromic-substring.spec.ts
@@ -2,9 +2,15 @@ import { expect, test } from '@jest/globals';
 import longestPalindrome from './longest-palindromic-substring';
 
 test('Input examples', () => {
-    expect(longestPalindrome('babad')).toBe('bab')
-    expect(longestPalindrome('cbbd')).toBe('bb')
-    expect(longestPalindrome('a')).toBe('a')
-    expect(longestPalindrome('ac')).toBe('a')
-    expect(longestPalindrome('aca')).toBe('aca')
+  expect(longestPalindrome('babad')).toBe('bab');
+  expect(longestPalindrome('cbbd')).toBe('bb');
+  expect(longestPalindrome('a')).toBe('a');
+  expect(longestPalindrome('ac')).toBe('a');
+  expect(longestPalindrome('aca')).toBe('aca');
+  expect(
+    longestPalindrome(
+      'zudfweormatjycujjirzjpyrmaxurectxrtqedmmgergwdvjmjtstdhcihacqnothgttgqfywcpgnuvwglvfiuxteopoyizgehkwuvvkqxbnufkcbodlhdmbqyghkojrgokpwdhtdrwmvdegwycecrgjvuexlguayzcammupgeskrvpthrmwqaqsdcgycdupykppiyhwzwcplivjnnvwhqkkxildtyjltklcokcrgqnnwzzeuqioyahqpuskkpbxhvzvqyhlegmoviogzwuiqahiouhnecjwysmtarjjdjqdrkljawzasriouuiqkcwwqsxifbndjmyprdozhwaoibpqrthpcjphgsfbeqrqqoqiqqdicvybzxhklehzzapbvcyleljawowluqgxxwlrymzojshlwkmzwpixgfjljkmwdtjeabgyrpbqyyykmoaqdambpkyyvukalbrzoyoufjqeftniddsfqnilxlplselqatdgjziphvrbokofvuerpsvqmzakbyzxtxvyanvjpfyvyiivqusfrsufjanmfibgrkwtiuoykiavpbqeyfsuteuxxjiyxvlvgmehycdvxdorpepmsinvmyzeqeiikajopqedyopirmhymozernxzaueljjrhcsofwyddkpnvcvzixdjknikyhzmstvbducjcoyoeoaqruuewclzqqqxzpgykrkygxnmlsrjudoaejxkipkgmcoqtxhelvsizgdwdyjwuumazxfstoaxeqqxoqezakdqjwpkrbldpcbbxexquqrznavcrprnydufsidakvrpuzgfisdxreldbqfizngtrilnbqboxwmwienlkmmiuifrvytukcqcpeqdwwucymgvyrektsnfijdcdoawbcwkkjkqwzffnuqituihjaklvthulmcjrhqcyzvekzqlxgddjoir',
+    ),
+  ).toBe('gykrkyg');
+  expect(longestPalindrome('abb')).toBe('bb');
 });
