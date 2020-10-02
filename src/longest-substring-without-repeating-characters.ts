827993078f7255ef31eba6f2723f7f2b618b8bfc
arthurjdam <arthurjdam@gmail.com>
2020-10-02 14:02:24 -0700

    longest-substring-without-repeating-characters answer 1

--- src/longest-substring-without-repeating-characters.ts
+++ src/longest-substring-without-repeating-characters.ts
@@ -1,1 +1,17 @@
-export default function lengthOfLongestSubstring(s: string): number {}
+export default function lengthOfLongestSubstring(s: string): number {
+    let longest = '';
+    for(let i = 0; i < s.length; ++i) {
+        let current = '';
+        for(let j = i; j < s.length; ++j) {
+            if (current.indexOf(s[j]) === -1) {
+                current += s[j];
+            } else {
+                break;
+            }
+        }
+        if (current.length > longest.length) {
+            longest = current;
+        }
+    }
+    return longest.length;
+}
