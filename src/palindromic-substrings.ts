9388fd8a99a1d61907db881b2115613c85f2645a
arthurjdam <arthurjdam@gmail.com>
2020-10-03 11:56:07 -0700

    palindromic-substrings answer

--- src/palindromic-substrings.ts
+++ src/palindromic-substrings.ts
@@ -1,1 +1,26 @@
-export default function countSubstrings(s: string): number {}
+function isPalindrome(s: string): boolean {
+  let l = 0;
+  let r = s.length - 1;
+  while (l < r) {
+    if (s[l] !== s[r]) {
+      return false;
+    } else {
+      l++;
+      r--;
+    }
+  }
+  return true;
+}
+
+export default function countSubstrings(s: string): number {
+  let count = 0;
+  for (let i = 0; i < s.length; ++i) {
+    for (let j = i; j < s.length; ++j) {
+      const substr = s.substr(i, s.length - j);
+      if (isPalindrome(substr)) {
+        count++;
+      }
+    }
+  }
+  return count;
+}
