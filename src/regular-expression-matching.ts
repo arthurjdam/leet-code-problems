f32e2a7e9479484f75f60e66d378e29f3797ddff
arthurjdam <arthurjdam@gmail.com>
2020-10-05 16:38:43 -0700

    regular-expression-match answer v2

--- src/regular-expression-matching.ts
+++ src/regular-expression-matching.ts
@@ -1,3 +1,30 @@
 export default function isMatch(s: string, p: string): boolean {
-  return new RegExp(`^${p}$`, 'gi').test(s);
+  const acc = [[true]];
+
+  for (let i = 1; i < p.length + 1; ++i) {
+    acc[0][i] = p[i - 1] === '*' ? acc[0][i - 2] : false;
+  }
+
+  for (let i = 1; i < s.length + 1; ++i) {
+    acc.push([]);
+    acc[i][0] = false;
+    for (let j = 1; j < p.length + 1; ++j) {
+      if (p[j - 1] === s[i - 1] || p[j - 1] === '.') {
+        acc[i][j] = acc[i - 1][j - 1];
+      } else if (p[j - 1] === '*') {
+        if (p[j - 2] === s[i - 1] || p[j - 2] === '.') {
+          acc[i][j] =
+            acc[i][j - 2] === true ||
+            acc[i][j - 1] === true ||
+            acc[i - 1][j] === true;
+        } else {
+          acc[i][j] = acc[i][j - 2];
+        }
+      } else {
+        acc[i][j] = false;
+      }
+    }
+  }
+  
+  return acc[s.length][p.length];
 }
