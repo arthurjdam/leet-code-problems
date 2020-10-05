2de4e4fe0fe4b65a7a513c7c10e3b0098627fc84
arthurjdam <arthurjdam@gmail.com>
2020-10-05 14:49:57 -0700

    Lint

--- src/regular-expression-matching.spec.ts
+++ src/regular-expression-matching.spec.ts
@@ -1,10 +1,10 @@
 import { expect, test } from '@jest/globals';
 import isMatch from './regular-expression-matching';
 
-test('Input examples', ()=> {
-    expect(isMatch('aa', 'a')).toBe(false);
-    expect(isMatch('aa', 'a*')).toBe(true);
-    expect(isMatch('ab', '.*')).toBe(true);
-    expect(isMatch('aab', 'c*a*b')).toBe(true);
-    expect(isMatch('mississippi', 'mis*is*p*.')).toBe(false);
+test('Input examples', () => {
+  expect(isMatch('aa', 'a')).toBe(false);
+  expect(isMatch('aa', 'a*')).toBe(true);
+  expect(isMatch('ab', '.*')).toBe(true);
+  expect(isMatch('aab', 'c*a*b')).toBe(true);
+  expect(isMatch('mississippi', 'mis*is*p*.')).toBe(false);
 });
