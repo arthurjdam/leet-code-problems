320c3765abfd364665f3d109c7d73b2e6a961a8d
arthurjdam <arthurjdam@gmail.com>
2020-10-05 14:04:04 -0700

    rotate-image answer

--- src/rotate-image.ts
+++ src/rotate-image.ts
@@ -2,5 +2,16 @@
  Do not return anything, modify matrix in-place instead.
  */
 export default function rotate(matrix: number[][]): void {
+  const l = matrix.length;
+  let swap;
 
-};
+  for (let i = 0; i < Math.floor(l / 2); ++i) {
+    for (let j = 0; j < Math.ceil(l / 2); ++j) {
+      swap = matrix[i][j];
+      matrix[i][j] = matrix[l - j - 1][i];
+      matrix[l - j - 1][i] = matrix[l - i - 1][l - j - 1];
+      matrix[l - i - 1][l - j - 1] = matrix[j][l - i - 1];
+      matrix[j][l - i - 1] = swap;
+    }
+  }
+}
