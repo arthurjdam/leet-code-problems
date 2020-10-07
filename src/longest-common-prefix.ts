3878142dd6771737f136eab98476534dce1b4c25
arthurjdam <arthurjdam@gmail.com>
2020-10-07 14:36:38 -0700

    longest-common-prefix answer (faster than 95%)

--- src/longest-common-prefix.ts
+++ src/longest-common-prefix.ts
@@ -1,1 +1,16 @@
-export default function longestCommonPrefix(strs: string[]): string {}
+export default function longestCommonPrefix(strs: string[]): string {
+  if (strs.length == 0) return '';
+  if (strs.length == 1) return strs[0];
+
+  const shortest = strs.sort((a, b) => a.length - b.length).shift() as string;
+  outer: for (let i = shortest.length; i > 0; --i) {
+    const current = shortest.substr(0, i);
+    for (let j = 0; j < strs.length; ++j) {
+      if (strs[j].substr(0, i) != current) {
+        continue outer;
+      }
+    }
+    return current;
+  }
+  return '';
+}
