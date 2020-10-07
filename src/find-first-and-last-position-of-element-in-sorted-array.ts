a653c54c46df3011a7da1fe5720577ce127e5a49
arthurjdam <arthurjdam@gmail.com>
2020-10-07 12:36:51 -0700

    find-first-and-last-position-of-element-in-sorted-array answer

--- src/find-first-and-last-position-of-element-in-sorted-array.ts
+++ src/find-first-and-last-position-of-element-in-sorted-array.ts
@@ -1,1 +1,14 @@
-export default function searchRange(nums: number[], target: number): number[] {}
+export default function searchRange(nums: number[], target: number): number[] {
+  let [start, end] = [-1, -1];
+  for (let i = 0; i < nums.length; ++i) {
+    if (nums[i] === target) {
+      if (start < 0) {
+        start = i;
+      }
+      end = i;
+    } else if (nums[i] > target) {
+      break;
+    }
+  }
+  return [start, end];
+}
