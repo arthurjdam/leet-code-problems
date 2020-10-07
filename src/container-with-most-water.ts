bef4f9ebdef4d552556d117016e63bd5f8e114e6
arthurjdam <arthurjdam@gmail.com>
2020-10-07 14:23:46 -0700

    container-with-most-water answer (though not very performant)

--- src/container-with-most-water.ts
+++ src/container-with-most-water.ts
@@ -1,1 +1,12 @@
-export default function maxArea(height: number[]): number {}
+export default function maxArea(height: number[]): number {
+    let largest = 0;
+    for(let i = height.length - 1; i > 0; --i) {
+        for(let j = 0; j < i; ++j) {
+            const area = (i - j) * Math.min(height[i], height[j])
+            if (area > largest) {
+                largest = area;
+            }
+        }
+    }
+    return largest;
+}
