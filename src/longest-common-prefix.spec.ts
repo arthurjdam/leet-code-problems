8437df5affb3043d41720ec9afb543ce4cf77b29
arthurjdam <arthurjdam@gmail.com>
2020-10-07 14:36:22 -0700

    extra tests

--- src/longest-common-prefix.spec.ts
+++ src/longest-common-prefix.spec.ts
@@ -4,4 +4,8 @@ import longestCommonPrefix from './longest-common-prefix';
 test('Input examples', () => {
   expect(longestCommonPrefix(['flower', 'flow', 'flight'])).toBe('fl');
   expect(longestCommonPrefix(['dog', 'racecar', 'car'])).toBe('');
+  expect(longestCommonPrefix([])).toBe('');
+  expect(longestCommonPrefix(['a'])).toBe('a');
+  expect(longestCommonPrefix(['a', 'ab'])).toBe('a');
+  expect(longestCommonPrefix(['ab', 'a'])).toBe('a');
 });
