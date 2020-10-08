2965bad188ccd1f2b5b405aa718ecdcecd5f7974
arthurjdam <arthurjdam@gmail.com>
2020-10-08 13:27:08 -0700

    additional test

--- src/3sum.spec.ts
+++ src/3sum.spec.ts
@@ -2,10 +2,14 @@ import { expect, test } from '@jest/globals';
 import threeSum from './3sum';
 
 test('Input examples', () => {
-  expect(threeSum([-1, 0, 1, 2, -1, -4])).toEqual([
+  expect(threeSum([-1, 0, 1, 2, -1, -4]).sort()).toEqual([
     [-1, -1, 2],
     [-1, 0, 1],
-  ]);
+  ].sort());
   expect(threeSum([])).toEqual([]);
   expect(threeSum([0])).toEqual([]);
+});
+
+test('Additional examples', () => {
+    expect(threeSum([1,2,-2,-1]).sort()).toEqual([]);
 });
