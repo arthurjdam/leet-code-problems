e5e80010c271fb01f0bb7b3e8ad2df70d05584be
arthurjdam <arthurjdam@gmail.com>
2020-10-08 15:59:36 -0700

    4sum answer 1

--- src/4sum.ts
+++ src/4sum.ts
@@ -1,1 +1,35 @@
-export default function fourSum(nums: number[], target: number): number[][] {}
+export default function fourSum(nums: number[], target: number): number[][] {
+  nums = nums.sort((a, b) => a - b);
+
+  const out: number[][] = [];
+
+  if (nums.length < 3) {
+    return out;
+  }
+
+  for (let i = 0; i < nums.length - 3; ++i) {
+    for (let j = i + 1; j < nums.length - 2; ++j) {
+      let k = j + 1;
+      let l = nums.length - 1;
+      while (k < l) {
+        const sum = nums[i] + nums[j] + nums[k] + nums[l];
+        if (sum === target) {
+          out.push([nums[i], nums[j], nums[k], nums[l]]);
+
+          while (nums[k] === nums[k + 1]) k++;
+          while (nums[l] === nums[l - 1]) l--;
+
+          k++;
+          l--;
+        } else if (sum < target) {
+          k++;
+        } else if (sum > target) {
+          l--;
+        }
+      }
+      while (nums[j] === nums[j + 1]) j++;
+    }
+    while (nums[i] === nums[i + 1]) i++;
+  }
+  return out;
+}
