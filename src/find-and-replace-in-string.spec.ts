f93d603ddeb4a55e6593157259aeb172a6246578
arthurjdam <arthurjdam@gmail.com>
2020-10-08 18:21:11 -0700

    extra test

--- src/find-and-replace-in-string.spec.ts
+++ src/find-and-replace-in-string.spec.ts
@@ -2,7 +2,10 @@ import { expect, test } from '@jest/globals';
 import findReplaceString from './find-and-replace-in-string';
 
 test('Input examples', () => {
-  expect(findReplaceString('abcd', [0, 2], ['a', 'bc'], ['eee', 'ffff'])).toBe(
+  expect(findReplaceString('abcd', [0, 1], ['a', 'bc'], ['eee', 'ffff'])).toBe(
+    'eeeffffd',
+  );
+  expect(findReplaceString('abcd', [0, 2], ['a', 'cd'], ['eee', 'ffff'])).toBe(
     'eeebffff',
   );
   expect(findReplaceString('abcd', [0, 2], ['ab', 'ec'], ['eee', 'ffff'])).toBe(
