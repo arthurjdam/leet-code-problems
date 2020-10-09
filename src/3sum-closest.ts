e2a219495c07134cad5e5749497d0885e8a3cc11
arthurjdam <arthurjdam@gmail.com>
2020-10-09 15:25:50 -0700

    3sum-closest answer

--- src/3sum-closest.ts
+++ src/3sum-closest.ts
@@ -1,4 +1,28 @@
 export default function threeSumClosest(
   nums: number[],
   target: number,
-): number {}
+): number {
+  nums = nums.sort((a, b) => a - b);
+
+  let current = nums[0] + nums[1] + nums[2];
+
+  for (let i = 0; i < nums.length - 2; ++i) {
+    let j = i + 1;
+    let k = nums.length - 1;
+    while (j < k) {
+      const sum = nums[i] + nums[j] + nums[k];
+      if (sum === target) {
+        return target;
+      } else if (sum < target) {
+        j++;
+      } else {
+        k--;
+      }
+      if (Math.abs(current - target) > Math.abs(sum - target)) {
+        current = sum;
+      }
+    }
+    while (nums[i] === nums[i + 1]) i++;
+  }
+  return current;
+}
