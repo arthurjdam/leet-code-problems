4cc20c4d70d0570207f0df699b4966f5042f1390
arthurjdam <arthurjdam@gmail.com>
2020-10-09 15:25:46 -0700

    extra test

--- src/3sum-closest.spec.ts
+++ src/3sum-closest.spec.ts
@@ -3,4 +3,6 @@ import threeSumClosest from './3sum-closest';
 
 test('Input examples', () => {
   expect(threeSumClosest([-1, 2, 1, -4], 1)).toBe(2);
+  expect(threeSumClosest([-4, -1, 2, 1], 1)).toBe(2);
+  expect(threeSumClosest([-100, -98, -2, -1], -101)).toBe(-101);
 });
