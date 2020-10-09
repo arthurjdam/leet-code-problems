402a13a1f43536ee70c43ff7d93bf7e1af6fa52e
arthurjdam <arthurjdam@gmail.com>
2020-10-08 18:21:14 -0700

    find-and-replace-in-string answer

--- src/find-and-replace-in-string.ts
+++ src/find-and-replace-in-string.ts
@@ -3,4 +3,19 @@ export default function findReplaceString(
   indexes: number[],
   sources: string[],
   targets: string[],
-): string {}
+): string {
+  let out = '';
+  for (let i = 0; i < S.length; ++i) {
+    const index = indexes.indexOf(i);
+    if (
+      index > -1 &&
+      S.substr(i, sources[index]?.length) === sources[index]
+    ) {
+      out += targets[index];
+      i += sources[index]?.length - 1;
+    } else {
+      out += S.charAt(i);
+    }
+  }
+  return out;
+}
