5036cd43de991738000eea4ff6b2cd4d8094335a
arthurjdam <arthurjdam@gmail.com>
2020-10-12 13:17:21 -0700

    Refactor

--- src/3sum-closest.ts
+++ src/3sum-closest.ts
@@ -4,7 +4,7 @@ export default function threeSumClosest(
 ): number {
   nums = nums.sort((a, b) => a - b);
 
-  let current = nums[0] + nums[1] + nums[2];
+  let closestSum = nums[0] + nums[1] + nums[2];
 
   for (let i = 0; i < nums.length - 2; ++i) {
     let j = i + 1;
@@ -18,11 +18,11 @@ export default function threeSumClosest(
       } else {
         k--;
       }
-      if (Math.abs(current - target) > Math.abs(sum - target)) {
-        current = sum;
+      if (Math.abs(closestSum - target) > Math.abs(sum - target)) {
+        closestSum = sum;
       }
     }
     while (nums[i] === nums[i + 1]) i++;
   }
-  return current;
+  return closestSum;
 }
