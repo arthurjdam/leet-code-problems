f7d5cb5e190cb3ec841470b464e2b899631722fd
arthurjdam <arthurjdam@gmail.com>
2020-10-13 20:03:36 -0700

    Lint

--- src/valid-parentheses.ts
+++ src/valid-parentheses.ts
@@ -3,6 +3,7 @@ export default function isValid(s: string): boolean {
     return s_.replace('{}', '').replace('[]', '').replace('()', '');
   }
 
+  // eslint-disable-next-line no-constant-condition
   while (true) {
     const s_ = reduce(s);
     if (s_.length === 0) return true;
